070b9b7a30f813688b5452f15bb1ef62ef039970
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-12 17:17:48 -0500

    changed <a> to <Link> | pic is routed to home

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,23 +1,44 @@
 import React, {Component} from 'react';
+import { withRouter } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import picture from '../../img/profile-pic.jpeg';
+import { Link } from 'react-router-dom';
 
 class Nav extends Component {
 
+  navToHome = () => {
+    this.props.history.push( '/' );
+  }
+
   render() {
     return (
-      <nav className="main-nav">
-        <img src={picture} alt="profile picture" className="nav-pic" ></img>
-        <ul>
-          <li><a href="#"><FontAwesomeIcon icon="home" /> Home</a></li>
-          <li><a href="#"><FontAwesomeIcon icon="info-circle" /> About</a></li>
-          <li><a href="#"><FontAwesomeIcon icon="people-carry" /> Services</a></li>
-          <li><a href="#"><FontAwesomeIcon icon={["far", "address-book"]} /> Contact</a></li>
-        </ul>
+      <nav className="main-nav" >
+        <div>
+            <div className="nav-pic-frame" onClick={ this.navToHome }>
+              <img
+                src={picture}
+                alt="Chase profile" className="nav-pic" >
+              </img>
+            </div>
+          <ul>
+            <li>
+              <Link to="/" ><FontAwesomeIcon icon="home" /> Home</Link>
+            </li>
+            <li>
+              <Link to="/" ><FontAwesomeIcon icon="info-circle" /> About</Link>
+            </li>
+            <li>
+              <Link to="/" ><FontAwesomeIcon icon="people-carry" /> Github</Link>
+            </li>
+            <li>
+              <Link to="/" ><FontAwesomeIcon icon={["far", "address-book"]} /> Contact</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
 }
 
-export default (Nav);
+export default (withRouter( Nav ));
 
