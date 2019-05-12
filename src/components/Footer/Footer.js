d8dfae1e0aa2602b79303da4b464be6e5e9acf8a
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-12 17:17:06 -0500

    init

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './Footer.css'
+import React, {Component} from 'react';
+import { withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import picture from '../../img/profile-pic.jpeg';
+import { Link } from 'react-router-dom';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+class Footer extends Component {
 
-const Footer = () => (
-  <footer>
-    footer
-  </footer>
-);
+  render() {
+    return (
+      <footer>
+        <p>Current work availability: Looking for opportunities. Will be available to start work Monday, July 1 2019.</p>
+      </footer>
+    );
+  }
+}
 
-export default Footer;
+export default (withRouter( Footer ));
