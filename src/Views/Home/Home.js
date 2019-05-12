1f5448653c0ab46edd60e8d46dafe58dfb06b01b
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-12 17:16:30 -0500

    new grid layout

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Nav from '../../components/Nav/Nav';
 import TacitBoxes from '../../components/TacitBoxes/TacitBoxes';
 import Testimonial from '../../components/Testimonial/Testimonial';
@@ -9,54 +8,48 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
-        <div className="wrapper">
+      <div className="wrapper" >
         {/* Navigation */}
-        <Nav/>
+        <Nav />
+        <div>
+          <div className="content-container" >
+            {/* Top Container */}
+            <section className="top-container" >
+              <header className="showcase" >
+                <h1>Chase Linzmeyer</h1>
+                <p>Full stack web developer. Always learning, always growing.</p>
+              </header>
+              {/* <div className="top-box top-box-a">
+                <h4>Membership</h4>
+                <p className="price">$199/mo</p>
+                <a href="" className="btn">Buy Now</a>
+              </div>
+              <div className="top-box top-box-b">
+                <h4>Pro Membership</h4>
+                <p className="price">$299/mo</p>
+                <a href="" className="btn">Buy Now</a>
+              </div> */}
+            </section>
+            
+            <section className="bottom-container" >
 
-        {/* Top Container */}
-        <section className="top-container">
-          <header className="showcase">
-            <h1>Chase Linzmeyer</h1>
-            <p>Full stack web developer. Always learning, always growing.</p>
-            <a href="#" className="btn" >Read More</a>
-          </header>
-          <div className="top-box top-box-a">
-            <h4>Membership</h4>
-            <p className="price">$199/mo</p>
-            <a href="" className="btn">Buy Now</a>
+              {/* { Boxes that describe my work ethic values } */}
+              <TacitBoxes />
+
+              {/* Testimonials change based on clicked TacitBoxes */}
+              <Testimonial />
+            </section>
+
+            {/* Portfolio */}
+            
+
+            {/* Footer */}
+
+            {/* Wrapper ends */}
+            </div>
+
           </div>
-          <div className="top-box top-box-b">
-            <h4>Pro Membership</h4>
-            <p className="price">$299/mo</p>
-            <a href="" className="btn">Buy Now</a>
-          </div>
-        </section>
-
-        {/* { Boxes that describe my work ethic values } */}
-        <TacitBoxes />
-
-        {/* Testimonials change based on clicked TacitBoxes */}
-        <Testimonial />
-
-        {/* Portfolio */}
-        <section className="portfolio">
-          <img src="https://source.unsplash.com/random/200x200" alt="..." />
-          <img src="https://source.unsplash.com/random/201x200" alt="..." />
-          <img src="https://source.unsplash.com/random/202x200" alt="..." />
-          <img src="https://source.unsplash.com/random/203x200" alt="..." />
-          <img src="https://source.unsplash.com/random/204x200" alt="..." />
-          <img src="https://source.unsplash.com/random/205x200" alt="..." />
-          <img src="https://source.unsplash.com/random/206x200" alt="..." />
-          <img src="https://source.unsplash.com/random/207x200" alt="..." />
-          <img src="https://source.unsplash.com/random/208x200" alt="..." />
-        </section>
-
-        {/* Footer */}
-
-        {/* Wrapper ends */}
         </div>
-      </div>
     );
   }
 }
