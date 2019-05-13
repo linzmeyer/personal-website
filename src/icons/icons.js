219675e8ec5094e894f6c65b32a2864ec3c341ee
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-13 00:57:17 -0500

    added icons

--- src/icons/icons.js
+++ src/icons/icons.js
@@ -5,7 +5,7 @@ import { library as icons } from '@fortawesome/fontawesome-svg-core'
 
 // import icons from dependencies
 
-
+// Solid svg's
 import { 
   faAddressCard,
   faAnchor,
@@ -15,44 +15,68 @@ import {
   faCog,
   faCopyright,
   faCircle,
+  faDatabase,
   faFire,
   faGlobe,
   faHome,
   faInfoCircle,
   faPeopleCarry,
+  faRetweet,
+  faSeedling,
+  faShippingFast,
   faSlidersH,
   faStroopwafel,
+  faTh,
   faUsers,
+  faCode,
 } from '@fortawesome/free-solid-svg-icons';
 
+// Regular svg's
 import {
   faAddressBook,
   faCalendarAlt,
+  faCalendarCheck,
   faHeart,
 } from '@fortawesome/free-regular-svg-icons';
 
-import { fab } from '@fortawesome/free-brands-svg-icons';
+// Brand svg's
+import {
+  faFortAwesome,
+  faGithub,
+  faNode,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons';
 
 // add individual icons to library
 icons.add(
   faAddressBook,
   faAddressCard,
   faAnchor,
-  fab,
   faCalendarAlt,
+  faCalendarCheck,
   faChartPie,
   faCheckSquare, 
+  faCode,
   faCoffee,
   faCog,
   faCopyright,
   faCircle,
+  faDatabase,
   faFire,
+  faFortAwesome,
+  faGithub,
   faGlobe,
   faHeart,
   faHome,
   faInfoCircle,
   faPeopleCarry,
+  faNode,
+  faReact,
+  faRetweet,
+  faSeedling,
   faSlidersH,
+  faShippingFast,
   faStroopwafel,
+  faTh,
   faUsers,
 );
